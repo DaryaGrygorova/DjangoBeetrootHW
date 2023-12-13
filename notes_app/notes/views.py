a8ad104329a47086301d247e907d893ba8a426ac
DaryaGrygorova <grygorova.darya@gmail.com>
2023-12-13 22:46:11 +0200

    Add base.html, notes template, add css styles. Make migrations

--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+NOTES = [
+    {
+        'id': 1,
+        'title': "Note 1",
+        'description': "Do something"
+    },
+    {
+        'id': 2,
+        'title': "Note 2",
+        'description': "Do another something"
+    },
+    {
+        'id': 3,
+        'title': "Note 3",
+        'description': "Just Do It!"
+    }
+]
 
 def notes(request):
-    return HttpResponse("Hello from Notes app!")
+    return render(request, 'notes/notes.html', {'notes': NOTES})
